ac3688b2e83039b683d5353820a2d4ca946d7999
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-02-23 16:01:38 -0300

    Update 23/02/19

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var txtNumber = document.getElementById('txtNumber'),
+    txtInterval = document.getElementById('txtInterval'),
     btnGenerate = document.getElementById('btnGenerate'),
     ans = document.getElementById('ans');
 
+btnGenerate.addEventListener('click', onClick);
 
-btnGenerate.addEventListener('click', generate);
-
-function generate() {
+function onClick() {
     let count = 0;
     if (!validate(txtNumber.value)) {
-        while (count <= 10) {
+        while (count <= txtInterval.value) {
             ans.innerHTML += parseFloat(txtNumber.value) + ' X ' + count + ' = ' + (parseFloat(txtNumber.value) * count) + '<br>';
             count++;
         }
@@ -19,5 +19,6 @@ function generate() {
 function validate(number) {
     let ret = false;
     if (number == '') ret = true;
+    else if (number != '' && ans != '') ans.innerHTML = '';
     return ret;
 }
