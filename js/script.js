433c5f7ad7f6e6c229cd608f13d6cde7475e05f8
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2019-02-25 15:10:31 -0300

    Update 25/02/19

--- js/script.js
+++ js/script.js
@@ -3,22 +3,43 @@ var txtNumber = document.getElementById('txtNumber'),
     btnGenerate = document.getElementById('btnGenerate'),
     ans = document.getElementById('ans');
 
+document.addEventListener('DOMContentLoaded', onFocus);
 btnGenerate.addEventListener('click', onClick);
+txtInterval.addEventListener('keydown', onKeyDown);
+txtNumber.addEventListener('keydown', onKeyDown);
 
 function onClick() {
     let count = 0;
-    if (!validate(txtNumber.value)) {
+    if (!validateNumber(txtNumber.value)) {
         while (count <= txtInterval.value) {
             ans.innerHTML += parseFloat(txtNumber.value) + ' X ' + count + ' = ' + (parseFloat(txtNumber.value) * count) + '<br>';
             count++;
         }
+        validateInterval(txtInterval.value);
     }
     return count;
 }
 
-function validate(number) {
+function validateNumber(number) {
     let ret = false;
     if (number == '') ret = true;
     else if (number != '' && ans != '') ans.innerHTML = '';
     return ret;
+}
+
+function validateInterval(interval) {
+    let ret = false;
+    if (interval > 100) {
+        ans.innerHTML = '';
+        ret = true;
+    }
+    return ret;
+}
+
+function onKeyDown(e) {
+    if (e.key === 'e') e.preventDefault();
+}
+
+function onFocus() {
+    txtNumber.focus();
 }
